8d282228ab9219465f8cee98d604a45eed554b4c
povermanr <rachel.poverman@gmail.com>
2024-01-15 17:46:06 -0500

    deck of cards complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [deckId, setDeckId] = useState(null);
+  const [remaining, setRemaining] = useState(0);
+  const [shuffling, setShuffling] = useState(false);
+  const [drawnCard, setDrawnCard] = useState(null);
+
+  useEffect(() => {
+    fetch('https://deckofcardsapi.com/api/deck/new/shuffle/')
+      .then((response) => response.json())
+      .then((data) => {
+        setDeckId(data.deck_id);
+        setRemaining(data.remaining);
+      });
+  }, []);
+
+  const drawCard = () => {
+    if (remaining > 0) {
+      fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/`)
+        .then((response) => response.json())
+        .then((data) => {
+          setRemaining(data.remaining);
+          setDrawnCard(data.cards[0]);
+        });
+    } else {
+      alert('Error: no cards remaining!');
+    }
+  };
+
+  const shuffleDeck = () => {
+    if (!shuffling) {
+      setShuffling(true);
+      fetch(`https://deckofcardsapi.com/api/deck/${deckId}/shuffle/`)
+        .then((response) => response.json())
+        .then(() => {
+          setRemaining(52);
+          setDrawnCard(null);
+          setShuffling(false);
+        });
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <div className="card">{drawnCard ? <img src={drawnCard.image} alt="drawn card" /> : null}</div>
+      <div>
+        <button className="action-button" onClick={drawCard}>
+          Draw a Card
+        </button>
+        <button className="action-button" onClick={shuffleDeck} disabled={shuffling}>
+          Shuffle Deck
+        </button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
